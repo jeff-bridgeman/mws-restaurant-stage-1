605340ba4aba0baafd6ec88563ff5fda887539bb
Jeff Bridgeman <jeff.bridgeman@outlook.com>
2018-07-09 22:35:01 -0500

    feat: fixed restaurant page and set breadcrumb aria

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -144,6 +144,7 @@ fillBreadcrumb = (restaurant=self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
+  li.setAttribute('aria-label',restaurant.name);
   breadcrumb.appendChild(li);
 }
 
